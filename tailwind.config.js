94a5bc1499d458122d401d29674f0753571f54a4
priapambudi <priapambudi97@gmail.com>
2024-04-09 14:46:18 +0700

    add dark-mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
